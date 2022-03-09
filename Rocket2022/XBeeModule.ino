29314883bd338e7256a995f674918e6a0cfdd24f
byalcin24 <83987766+byalcin24@users.noreply.github.com>
2022-03-09 14:59:49 +0300

    Update XBeeModule.ino

--- Rocket2022/XBeeModule.ino
+++ Rocket2022/XBeeModule.ino
@@ -1,37 +1,40 @@
  //Function for reading byte
+int ReadByte(CString PortSpecifier)
+{
+ DCB dcb;
+ int retVal;
+ BYTE Byte;
+ DWORD dwBytesTransferred;
+ DWORD dwCommModemStatus;
+ HANDLE hPort = CreateFile(
 
-int ReadByte(String PortSpecifier){
-  //Initializations
-  //DCB dcb;
-  int retVal;
-  byte Byte;
-  unsigned int dwBytesTransferred;
-  unsigned int dwCommModemStatus;
-  
-  PortSpecifier.c_str();
-  
-  void* hPort = SDOpen(PortSpecifier, FILE_READ )
-  );
-  //Current control settings for the comm device
-  if (!GetCommState(hPort,&dcb))
-    return 0x100;
-  dcb.BaudRate = CBR_9600; //9600 Baud
-  dcb.ByteSize = 8; //8 data bits
-  dcb.Parity = NOPARITY; //no parity
-  dcb.StopBits = ONESTOPBIT; //1 stop
-  if (!SetCommState(hPort,&dcb))
-    return 0x100;
-  SetCommMask (hPort, EV_RXCHAR | EV_ERR); //receive character event
-  WaitCommEvent (hPort, &dwCommModemStatus, 0); //wait for character
-  if (dwCommModemStatus & EV_RXCHAR)
-    ReadFile (hPort, &Byte, 1, &dwBytesTransferred, 0); //read 1
-  
-  else if (dwCommModemStatus & EV_ERR)
-  retVal = 0x101;
-  retVal = Byte;
-  CloseHandle(hPort);
-  return retVal;
-} 
+ PortSpecifier,
+ GENERIC_READ,
+ 0,
+ NULL,
+ OPEN_EXISTING,
+ 0,
+ NULL
+ );
+ if (!GetCommState(hPort,&dcb))
+ return 0x100;
+ dcb.BaudRate = CBR_9600; //9600 Baud
+ dcb.ByteSize = 8; //8 data bits
+ dcb.Parity = NOPARITY; //no parity
+ dcb.StopBits = ONESTOPBIT; //1 stop
+ if (!SetCommState(hPort,&dcb))
+ return 0x100;
+ SetCommMask (hPort, EV_RXCHAR | EV_ERR); //receive character event
+ WaitCommEvent (hPort, &dwCommModemStatus, 0); //wait for character
+ if (dwCommModemStatus & EV_RXCHAR)
+ ReadFile (hPort, &Byte, 1, &dwBytesTransferred, 0); //read 1
+ else if (dwCommModemStatus & EV_ERR)
+ retVal = 0x101;
+ retVal = Byte;
+ CloseHandle(hPort);
+ serial.println(RetVal);
+ return retVal;
+}
 
 void setup() {
   // put your setup code here, to run once:
